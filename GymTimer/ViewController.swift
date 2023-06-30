687ff16305cdac8e028103e268c32868ff32d4ca
陳逸煌 <joh1234567892001@gmail.com>
2023-06-30 08:15:36 +0800

    Initial Commit

---
+++ GymTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GymTimer
+//
+//  Created by 陳逸煌 on 2023/6/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
